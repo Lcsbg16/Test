e617ae7fff304599aea193ac8c802171dec81665
Lucas <lbguimaraes16@gmail.com>
2022-11-14 17:13:36 -0300

    correcao

--- application/models/LeiturasModel.php
+++ application/models/LeiturasModel.php
@@ -4,16 +4,45 @@ require_once 'BaseModel.php';
 
 class LeiturasModel extends BaseModel
 {
-
+        public function getUltimaTemperaturaMedia()
+    {
+        $this->db->from('estacao E')
+                ->select("
+                    AVG(
+                        (
+                            SELECT
+                                    temperatura
+                            FROM
+                                    leitura L1
+                            WHERE
+                                    L1.estacao_id = E.id
+                                    AND datahora >= '" . date('Y-m-d') . " 00:00:00'
+                            ORDER BY
+                                    datahora DESC
+                            LIMIT 1
+                        )
+                    )
+                    AS temperatura_media
+                ");
+        $linha = $this->db->get()->row_array();
+        return $linha['temperatura_media'];
+    }
+    
     public function getVolumeChuvaMinimo(){
         $this->db->from("Leitura L")
                 ->select("
                     MIN(
                         (
                             SELECT
-                                volume_chuva
+                                    volume_chuva
                             FROM
-                                leitura L
+                                    leitura L1
+                            WHERE
+                                    L1.estacao_id = E.id
+                                    AND datahora >= '" . date('Y-m-d') . " 00:00:00'
+                            ORDER BY
+                                    datahora DESC
+                            LIMIT 1                                    
                     )
                     AS volume_minimo
                     ");
@@ -28,9 +57,15 @@ class LeiturasModel extends BaseModel
                     MAX(
                         (
                             SELECT
-                                volume_chuva
+                                    volume_chuva
                             FROM
-                                leitura L
+                                    leitura L1
+                            WHERE    
+                                    L1.estacao_id = E.id
+                                    AND datahora >= '" . date('Y-m-d') . " 00:00:00'
+                            ORDER BY
+                                    datahora DESC
+                            LIMIT 1
                     )
                     AS volume_maxima
                     ");
--- application/views/smarty/templates/Dashboard/index.tpl
+++ application/views/smarty/templates/Dashboard/index.tpl
@@ -83,7 +83,7 @@
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Volume Min. Chuva</h5>
-                                    <span class="h2 font-weight-bold mb-0">{$vol_chuva_min}mm</span>
+                                    <span class="h2 font-weight-bold mb-0">{$vol_chuva_min|number_format:1:","}mm</span>
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-gradient-blue text-white rounded-circle shadow">
@@ -101,7 +101,7 @@
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Volume M&aacute;x. Chuva</h5>
-                                    <span class="h2 font-weight-bold mb-0">{$vol_chuva_max}mm</span>
+                                    <span class="h2 font-weight-bold mb-0">{$vol_chuva_max|number_format:1:","}mm</span>
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-gradient-gray text-white rounded-circle shadow">
