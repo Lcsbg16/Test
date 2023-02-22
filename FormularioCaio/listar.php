c084289fbc3185673023162732a363a783194a0f
caioluscas <caiolucasrdz@gmail.com>
2023-02-21 21:02:55 -0300

    Tranformar esse codigo todo para MVC

--- FormularioCaio/listar.php
+++ FormularioCaio/listar.php
@@ -1,13 +1,38 @@
 <?php
-include_once("conexao.php");
-if(isset($_POST['search'])) {
-    $search = $_POST['search'];
-    $result_pessoa = "SELECT * from pessoa WHERE nome LIKE '%$search%' OR cpf LIKE '%$search%'";
-} else {
-    $result_pessoa = "SELECT * from pessoa";
-}
-$resultado_pessoa = mysqli_query($conn, $result_pessoa);
+    require_once("conexao.php");
+    // $sql = "SELECT * FROM pessoa";
+    // $resultado_pessoa = mysqli_query($conn,$sql);
 
+    $sql = "SELECT * FROM pessoa WHERE 1 ";
+
+    if(isset($_GET['buscar']) && $_GET['buscar'] == 1)
+    {
+        $nome = ($_GET['nome']);
+        $data_inicio = ($_GET['data_inicio']);
+        $data_fim = ($_GET['data_fim']);
+        $tipo_pessoa = ($_GET['tipo_pessoa']);
+        if($nome)
+        {
+            $sql .= " AND nome LIKE '%{$nome}%'";
+        }
+        
+        if($data_inicio)
+        {
+            $sql .= " AND data_nascimento >= '{$data_inicio}'";
+        }
+        
+        if($data_fim)
+        {
+            $sql .= " AND data_nascimento <= '{$data_fim}' ";
+        }
+
+        if($tipo_pessoa)
+        {
+            $sql .= " AND tipo_pessoa LIKE '%{$tipo_pessoa}%'";
+        }
+        
+    }
+    $resultado_pessoa = mysqli_query($conn,$sql);
 ?>
 
 <!DOCTYPE html>
@@ -19,34 +44,38 @@ $resultado_pessoa = mysqli_query($conn, $result_pessoa);
     <title>Listagem</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <style>
-            body{
-                text-align: center;
-            }
-            .search-box {
-                display: flex;
-                position: absolute;
-                top: 0;
-                right: 0;
-            }
-            .box-search{
-                width: 600px;
-                margin-left: 60%;
-                margin-right: 50%;
+            h1{
+                display:flex;
+                justify-content: center;   
             }
     </style>
 </head>
 <body>
-    <div class="box-search">
-        
-        <input type="search" class="form-control w-25" placeholder="Pesquisa" id="pesquisar">
-        <button onclick="searchData()"  class="btn btn-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi        bi-search-heart" viewBox="0 0 16 16">
-                <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
-                <path d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
-            </svg>
-        </button>
-        </div>
+    <div class="container">
+        <h1>Buscar Usuario</h1>
         <div>
+        <form action="listar.php" method="GET">
+            <input type="hidden" name="buscar" value="1">
+            Nome: <input type="text" name="nome" class="form-control" value="<?=isset($_GET['nome'])?$_GET['nome']:''?>">
+            Dt. Nascimento: <br> De: <input type="date" name="data_inicio" class="form-control" value="<?=isset($_GET['data_inicio'])?$_GET['data_inicio']:''?>"> A: <input class="form-control" type="date" name="data_fim" value="<?=isset($_GET['data_fim'])?$_GET['data_fim']:''?>">
+            <br>
+            Tipo de Pessoa:
+            <?php
+                $opcoes = ["-" => "", "PF" => "PF", "PJ" => "PJ" ];
+            ?>
+            <select class="form-control" name="tipo_pessoa" id="tipo_pessoa">
+                <?php foreach($opcoes as $label => $valor): ?>
+                    <option value="<?=$valor?>" <?=isset($tipo_pessoa) && $tipo_pessoa == $valor ? 'selected':""?>><?=$label?></option>
+                <?php endforeach;?>
+            </select> 
+            <br>        
+            <button type="button" class="btn btn-primary" onclick="window.location='<?=$_SERVER['PHP_SELF']?>'">Limpar</button>
+	        <button type="submit" class="btn btn-primary" >Buscar</button>
+        </form>
+    </div>
+        <div>
+        <hr>
+        <h1>Listar Usuario</h1>    
         <table class="table table-striped table-dark" >
             <thead class="dark">
                 <tr>
@@ -107,22 +136,12 @@ $resultado_pessoa = mysqli_query($conn, $result_pessoa);
             </tbody>
         </table>
     </div>
+    </div>
 </body>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
         <script src="js/personalizado.js"></script>
-        <script>
-            var search = document.getElementById('pesquisar');
-            search.addEventListener("keydown",function(event){
-                if(event.key === "Enter"){
-                    searchData();
-                }
-            });
-            function searchData(){  //Joga o valor digitado no input
-                window.location = 'listar.php?search='+search.value;
-            }  
-        </script>
 
 
 
