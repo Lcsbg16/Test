5c469e057abf47692f5d4b5008f0ede3348f284c
Adriano de Oliveira Gonçalves <adrianogoncalves@tic.ufrj.br>
2023-03-07 10:18:25 -0300

    Controller de usuário comum logado

---
+++ application/controllers/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+require_once 'BaseController.php';
+
+/**
+ * Controller padrão de ações de usuário comum
+ */
+class Usuario extends BaseController
+{
+
+}
