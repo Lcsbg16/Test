8026bdcaeebd18e8dc8343d3039d474ef2be6040
caioluscas <caiolucasrdz@gmail.com>
2023-07-11 22:07:18 -0300

    adicionado mqtt

--- FormularioCaio/confirmar_del.php
+++
@@ -1,6 +0,0 @@
-<?php
-$id = filter_input(INPUT_GET,'id',FILTER_SANITIZE_NUMBER_INT);
-
-echo 'Quer mesmo apagar esse registro?';
-echo "<a href='delete.php?id=$id&excluir=1'>Yes</a> |";
-echo "<a href='listar.php'> No</a>";
