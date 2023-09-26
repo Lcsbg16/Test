5bba4f72f959abf74f97490059b7580cfc4e170e
Adriano de Oliveira Gonçalves <contato@adrianoweb.com.br>
2023-09-26 06:58:00 -0300

    Correção na checagem dos dados da última leitura

--- application/models/EstacoesModel.php
+++ application/models/EstacoesModel.php
@@ -160,8 +160,24 @@ class EstacoesModel extends BaseModel
             if ($eAtual['latitude'] && $eAtual['longitude'])
             {
 
-                $ultimoRegistro = $this->getUltimoRegistro($eAtual['id']);
-                $estacoes[]     = [
+                if ($ultimoRegistro = $this->getUltimoRegistro($eAtual['id']))
+                {
+                    $ultimaLeituraRetorno = [
+                        'datahora'           => $ultimoRegistro['datahora'],
+                        'datahora_formatada' => date('d/m/Y H:i:s', strtotime($ultimoRegistro['datahora'])),
+                        'temperatura'        => $ultimoRegistro['temperatura'],
+                        'umidade_ar'         => $ultimoRegistro['umidade_ar'],
+                        'velocidade_vento'   => $ultimoRegistro['velocidade_vento'],
+                        'dir_vento'          => $ultimoRegistro['dir_vento'],
+                        'volume_chuva'       => $ultimoRegistro['volume_chuva']
+                    ];
+                }
+                else
+                {
+                    $ultimaLeituraRetorno = NULL;
+                }
+
+                $estacoes[] = [
                     'type'       => 'Feature',
                     'geometry'   => [
                         'type'        => 'Point',
@@ -172,15 +188,7 @@ class EstacoesModel extends BaseModel
                     ],
                     'properties' => [
                         'estacao'       => $eAtual,
-                        'ultimaLeitura' => [
-                            'datahora'           => $ultimoRegistro['datahora'] ?? NULL,
-                            'datahora_formatada' => date('d/m/Y H:i:s', strtotime($ultimoRegistro['datahora'])),
-                            'temperatura'        => $ultimoRegistro['temperatura'],
-                            'umidade_ar'         => $ultimoRegistro['umidade_ar'],
-                            'velocidade_vento'   => $ultimoRegistro['velocidade_vento'],
-                            'dir_vento'          => $ultimoRegistro['dir_vento'],
-                            'volume_chuva'       => $ultimoRegistro['volume_chuva']
-                        ],
+                        'ultimaLeitura' => $ultimaLeituraRetorno,
                         'camada'        => $this->getConfiguracoesCamada($ultimoRegistro, $camada)
                     ],
                     'id'         => $eAtual['id']
