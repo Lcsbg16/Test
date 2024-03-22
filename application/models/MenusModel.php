26bf0055c65950ebb30b5d35a4d9afdb15a2d5c4
Adriano de Oliveira Gonçalves <contato@adrianoweb.com.br>
2024-03-21 22:52:39 -0300

    Fix: Pequenos ajustes referentes à tela de visualização de estaçÕes
    Refs: 128

--- application/models/MenusModel.php
+++ application/models/MenusModel.php
@@ -15,7 +15,7 @@ class MenusModel extends BaseModel
         $principal->adicionarFilho(new MenuItem('Painel de Controle', base_url('Dashboard'), 'tv-2'));
         $principal->adicionarFilho(new MenuItem('Mapa de Estações', base_url('Estacoes/mapa'), 'map-big', 'yellow'));
         $principal->adicionarFilho(new MenuItem('Mapa de Monitoramento', base_url('Estacoes/mapaMonitoramento'), 'map-big', 'orange'));
-        $principal->adicionarFilho(new MenuItem('Visualizar Estações', base_url('Info'), 'grip', 'orange'));
+        $principal->adicionarFilho(new MenuItem('Visualizar Estações', base_url('Info'), 'archive-2', 'orange'));
 
         $principal->adicionarFilho(new MenuItem('Gráficos', base_url('grafico'), 'chart-bar-32', 'green'));
         $principal->adicionarFilho(new MenuItem('Reportar Ocorrência', base_url('AdminOcorrencias/index/add'), 'square-pin', 'orange'));
