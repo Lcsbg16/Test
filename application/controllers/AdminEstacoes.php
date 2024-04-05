2f43fde9b25817aa7a42efa07e8f4b675a98cd88
Lucas <lbguimaraes16@gmail.com>
2024-04-05 17:31:56 -0300

    Log das telas do grocery crud

--- application/controllers/AdminEstacoes.php
+++ application/controllers/AdminEstacoes.php
@@ -69,7 +69,7 @@ class AdminEstacoes extends BaseCrudController
         // Callbacks de processamento
         $crud->callback_before_insert(array($this, 'callbackBeforeProcess'));
         $crud->callback_before_update(array($this, 'callbackBeforeProcess'));
-
+        
         $this->_crud_output($crud);
     }
 
