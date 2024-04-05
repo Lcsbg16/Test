fcb9e59b54d64584284fe240ee7871c48d31693b
Jaque <raissadias.fac@gmail.com>
2024-04-05 17:49:51 -0300

    alteração no model

--- application/models/EstacoesModel.php
+++ application/models/EstacoesModel.php
@@ -173,7 +173,7 @@ class EstacoesModel extends BaseModel
      */
     public function getEstacoesGeoJson($camada = NULL, $somenteAtivas, $ids)
     {
-        $estacoesBD = $this->getEstacoes($somenteAtivas, $ids); //getEstações a partir dos ids
+        $estacoesBD = $this->getEstacoes($somenteAtivas, $ids); 
 
         $estacoes = [];
 
