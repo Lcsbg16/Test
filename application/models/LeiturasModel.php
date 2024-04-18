41ed2475b4661832cf6dd291dcab86dd8b248c76
Adriano de Oliveira Gonçalves <contato@adrianoweb.com.br>
2024-04-17 23:41:59 -0300

    fix: Telas com registros calculados estão muito lentas
    refs: 255

--- application/models/LeiturasModel.php
+++ application/models/LeiturasModel.php
@@ -751,7 +751,7 @@ class LeiturasModel extends BaseModel
         $registros = [];
         foreach ($estacoes as $estacaoAtual)
         {
-            $ultimoRegistroEstacao = $this->EstacoesModel->getUltimoRegistro($estacaoAtual['id']);
+            $ultimoRegistroEstacao = $this->EstacoesModel->getUltimoRegistro($estacaoAtual['id'], 60);
             if ($ultimoRegistroEstacao)
             {
                 $registros[] = $ultimoRegistroEstacao;
