b144de29d8e53c853ca8a8444702239d1bfe564e
Adriano de Oliveira Gonçalves <contato@adrianoweb.com.br>
2024-11-18 19:54:12 -0300

    fix: Erro na tela de cron

--- application/controllers/RelatorioLeituras.php
+++ application/controllers/RelatorioLeituras.php
@@ -4,26 +4,31 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 require_once 'BaseCrudController.php';
-require_once BASEPATH . '../application/models/LeiturasModel.php';
 
 class RelatorioLeituras extends BaseCrudController
 {
 
-
     private $objLeituraModel;
     private $fonteDadosController = "";
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("LeiturasModel");
+    }
+
     public function index()
     {
-       
-        if(defined('Fonte_RelatorioLeitura')){
-            $this->fonteDadosController = Fonte_RelatorioLeitura ;
+
+        if (defined('Fonte_RelatorioLeitura'))
+        {
+            $this->fonteDadosController = Fonte_RelatorioLeitura;
         }
 
-       $this->objLeituraModel = new LeiturasModel($this->fonteDadosController); 
+        $this->objLeituraModel = new LeiturasModel($this->fonteDadosController);
         $this->load->model('EstacoesModel');
-        $variaveisView = [];
-        $filtros       = new FiltrosLeitura(); //New filtro -> contato com o BD
+        $variaveisView         = [];
+        $filtros               = new FiltrosLeitura(); //New filtro -> contato com o BD
         if ($this->input->get('tipoInformacao'))
         {
             $estacoes             = $this->input->get('estacoes');
@@ -62,20 +67,18 @@ class RelatorioLeituras extends BaseCrudController
             }
             $filtros->setDirecao(FiltrosLeitura::DIRECAO_ASC);
             $leituras = $this->objLeituraModel->calcularEstatisticasPorPeriodo($filtros);
-            
         }
         else
         {
             $leituras = [];
         }
-        
+
         //$estacoes  = $this->EstacoesModel->getEstacoesComAcessoPorUsuario();
 
         $this->EstacoesModel->getArrayEstacoesComAcesso();
-               
 
         $variaveisView['estacoes']      = $this->EstacoesModel->getEstacoes(false, $this->EstacoesModel->estacoesComAcesso);
-       // $variaveisView['estacoes']      = $this->EstacoesModel->getEstacoes();
+        // $variaveisView['estacoes']      = $this->EstacoesModel->getEstacoes();
         $variaveisView['titulo_pagina'] = 'Estatísticas';
         $variaveisView['leituras']      = $leituras;
 
